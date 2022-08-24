c2ddf52c6cb4964aac67d7d9347293fd360820ee
ivanbiljan <ssbiljan@gmail.com>
2022-08-24 23:32:39 +0200

    Add empty IDatabaseCreator implementation

---
+++ src/DynamoDb.Linq/DynamoDbDatabaseCreator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace DynamoDb.Linq;
+
+public sealed class DynamoDbDatabaseCreator : IDatabaseCreator
+{
+    public bool EnsureDeleted() => throw new NotImplementedException();
+
+    public Task<bool> EnsureDeletedAsync(CancellationToken cancellationToken = new CancellationToken()) => throw new NotImplementedException();
+
+    public bool EnsureCreated() => throw new NotImplementedException();
+
+    public Task<bool> EnsureCreatedAsync(CancellationToken cancellationToken = new CancellationToken()) => throw new NotImplementedException();
+
+    public bool CanConnect() => throw new NotImplementedException();
+
+    public Task<bool> CanConnectAsync(CancellationToken cancellationToken = new CancellationToken()) => throw new NotImplementedException();
+}
