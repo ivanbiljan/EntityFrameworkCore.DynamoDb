79e4f14dfa2bfc5acaa9b49f477ea93f3213fbdc
ivanbiljan <ssbiljan@gmail.com>
2022-09-05 14:32:59 +0200

    Move property related extensions from EntityTypeExtensions into PropertyExtensions

--- src/DynamoDb.Linq/Extensions/EntityTypeExtensions.cs
+++ src/DynamoDb.Linq/Extensions/EntityTypeExtensions.cs
@@ -99,23 +99,6 @@ public static class EntityTypeExtensions
     }
 
     /// <summary>
-    /// Gets the name of the DynamoDb attribute the specified <paramref name="property"/> maps to.
-    /// </summary>
-    /// <param name="property">The property.</param>
-    /// <returns>The name of the DynamoDb attribute the property maps to.</returns>
-    public static string GetDynamoDbAttributeName(this IProperty property)
-    {
-        return property[Annotations.DynamoDbAttribute] as string ?? property.Name;
-    }
-    
-    /// <summary>
-    /// Sets the name of the DynamoDb attribute the specified <paramref name="property"/> maps to.
-    /// </summary>
-    /// <param name="property">The property.</param>
-    /// <param name="attributeName">The name of the attribute.</param>
-    public static void SetDynamoDbAttributeName(this IMutableProperty property, string attributeName) => property.SetAnnotation(Annotations.DynamoDbAttribute, attributeName);
-
-    /// <summary>
     /// Determines whether the given <see cref="IEntityType"/> is a document root. Document roots are types that have no owner.
     /// </summary>
     /// <param name="entityType">The entity type.</param>
