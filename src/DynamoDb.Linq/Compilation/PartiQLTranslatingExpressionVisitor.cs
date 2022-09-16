6e591e4b5c6c03ce47cd71f984a2fba56d4815ef
ivanbiljan <ssbiljan@gmail.com>
2022-09-17 00:47:50 +0200

    Add IPartiQLExpressionFactory

---
+++ src/DynamoDb.Linq/Compilation/PartiQLTranslatingExpressionVisitor.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq.Expressions;
+using DynamoDb.Linq.Syntax.Expressions;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace DynamoDb.Linq.Compilation;
+
+/// <summary>
+/// Represents a class that takes a LINQ expression tree and translates it into the equivalent PartiQL model.
+/// </summary>
+/// <remarks>This is the second step</remarks>
+internal sealed class PartiQLTranslatingExpressionVisitor : ExpressionVisitor
+{
+    private readonly IPartiQLExpressionFactory _partiQLExpressionFactory;
+
+    public PartiQLTranslatingExpressionVisitor(IPartiQLExpressionFactory partiQLExpressionFactory)
+    {
+        _partiQLExpressionFactory = partiQLExpressionFactory;
+    }
+
+    /// <summary>
+    /// Translates the provided expression into the equivalent PartiQL expression.
+    /// </summary>
+    /// <param name="expression">The expression.</param>
+    /// <returns>The parsed expression.</returns>
+    public PartiQLExpression? Translate(Expression? expression)
+    {
+        if (expression is null)
+        {
+            return null;
+        }
+
+        var result = Visit(expression);
+        if (result is not PartiQLExpression partiQLExpression)
+        {
+            return null;
+        }
+
+        return partiQLExpression;
+    }
+
+    protected override Expression VisitUnary(UnaryExpression node)
+    {
+        var operand = Visit(node) as PartiQLExpression;
+
+        return operand;
+    }
+}
--- src/DynamoDb.Linq/Syntax/Expressions/PartiQLExpressionFactory.cs
+++ src/DynamoDb.Linq/Syntax/Expressions/PartiQLExpressionFactory.cs
@@ -1,43 +1,126 @@
 ﻿using System.Linq.Expressions;
+using DynamoDb.Linq.Compilation;
 using Microsoft.EntityFrameworkCore.Storage;
 
 namespace DynamoDb.Linq.Syntax.Expressions;
 
 /// <summary>
-///     Represents a class that creates <see cref="PartiQLExpression" />s.
+/// Specifies a contract for a factory that produces PartiQL expressions.
 /// </summary>
-internal static class PartiQLExpressionFactory
+internal interface IPartiQLExpressionFactory
 {
-    public static PartiQLBinaryExpression AndAlso(PartiQLExpression left, PartiQLExpression right) =>
-        new(left, ExpressionType.AndAlso, right);
-
-    public static PartiQLBinaryExpression Equal(PartiQLExpression left, PartiQLExpression right) =>
-        new(left, ExpressionType.Equal, right);
-
-    public static PartiQLBinaryExpression GreaterThan(PartiQLExpression left, PartiQLExpression right) =>
-        new(left, ExpressionType.GreaterThan, right);
-
-    public static PartiQLBinaryExpression GreaterThanOrEqual(PartiQLExpression left, PartiQLExpression right) =>
-        new(left, ExpressionType.GreaterThanOrEqual, right);
-
-    public static PartiQLBinaryExpression LessThan(PartiQLExpression left, PartiQLExpression right) =>
-        new(left, ExpressionType.LessThan, right);
-
-    public static PartiQLBinaryExpression LessThanOrEqual(PartiQLExpression left, PartiQLExpression right) =>
-        new(left, ExpressionType.LessThanOrEqual, right);
+    /// <summary>
+    /// Produces a PartiQL AndAlso (&&) expression.
+    /// </summary>
+    /// <param name="left">The first operand.</param>
+    /// <param name="right">The second operand.</param>
+    /// <returns>The <see cref="PartiQLBinaryExpression"/>.</returns>
+    PartiQLBinaryExpression AndAlso(PartiQLExpression left, PartiQLExpression right);
+    
+    /// <summary>
+    /// Produces a PartiQL Equal (==) expression.
+    /// </summary>
+    /// <param name="left">The first operand.</param>
+    /// <param name="right">The second operand.</param>
+    /// <returns>The <see cref="PartiQLBinaryExpression"/>.</returns>
+    PartiQLBinaryExpression Equal(PartiQLExpression left, PartiQLExpression right);
+    
+    /// <summary>
+    /// Produces a PartiQL GreaterThan (>) expression.
+    /// </summary>
+    /// <param name="left">The first operand.</param>
+    /// <param name="right">The second operand.</param>
+    /// <returns>The <see cref="PartiQLBinaryExpression"/>.</returns>
+    PartiQLBinaryExpression GreaterThan(PartiQLExpression left, PartiQLExpression right);
+    
+    /// <summary>
+    /// Produces a PartiQL GreaterThanOrEqual (>=) expression.
+    /// </summary>
+    /// <param name="left">The first operand.</param>
+    /// <param name="right">The second operand.</param>
+    /// <returns>The <see cref="PartiQLBinaryExpression"/>.</returns>
+    PartiQLBinaryExpression GreaterThanOrEqual(PartiQLExpression left, PartiQLExpression right);
+    
+    /// <summary>
+    /// Produces a PartiQL LessThan (&lt;) expression.
+    /// </summary>
+    /// <param name="left">The first operand.</param>
+    /// <param name="right">The second operand.</param>
+    /// <returns>The <see cref="PartiQLBinaryExpression"/>.</returns>
+    PartiQLBinaryExpression LessThan(PartiQLExpression left, PartiQLExpression right);
+    
+    /// <summary>
+    /// Produces a PartiQL LessThanOrEqual (&lt;=) expression.
+    /// </summary>
+    /// <param name="left">The first operand.</param>
+    /// <param name="right">The second operand.</param>
+    /// <returns>The <see cref="PartiQLBinaryExpression"/>.</returns>
+    PartiQLBinaryExpression LessThanOrEqual(PartiQLExpression left, PartiQLExpression right);
 
     /// <summary>
-    ///     Produces an expression of type <see cref="ExpressionType.Constant"/>.
+    ///     Produces a PartiQL constant expression.
     /// </summary>
     /// <param name="value">The constant.</param>
     /// <param name="coreTypeMapping">The <see cref="CoreTypeMapping" />.</param>
-    /// <returns>The created <see cref="PartiQLConstantExpression" />.</returns>
-    public static PartiQLConstantExpression MakeConstant(object value, CoreTypeMapping? coreTypeMapping) =>
+    /// <returns>The <see cref="PartiQLConstantExpression" />.</returns>
+    PartiQLConstantExpression MakeConstant(object value, CoreTypeMapping? coreTypeMapping);
+    
+    /// <summary>
+    /// Produces a PartiQL NotEqual (!=) expression. In DynamoDb terms this is the NOT operator.
+    /// </summary>
+    /// <param name="left">The first operand.</param>
+    /// <param name="right">The second operand.</param>
+    /// <returns>The <see cref="PartiQLBinaryExpression"/>.</returns>
+    PartiQLBinaryExpression NotEqual(PartiQLExpression left, PartiQLExpression right);
+    
+    /// <summary>
+    /// Produces a PartiQL OrElse (||) expression. In DynamoDb terms this is the OR operator.
+    /// </summary>
+    /// <param name="left">The first operand.</param>
+    /// <param name="right">The second operand.</param>
+    /// <returns>The <see cref="PartiQLBinaryExpression"/>.</returns>
+    PartiQLBinaryExpression OrElse(PartiQLExpression left, PartiQLExpression right);
+}
+
+
+/// <summary>
+///     Represents a class that creates <see cref="PartiQLExpression" />s.
+/// </summary>
+internal class PartiQLExpressionFactory : IPartiQLExpressionFactory
+{
+    /// <inheritdoc />
+    public PartiQLBinaryExpression AndAlso(PartiQLExpression left, PartiQLExpression right) =>
+        new(left, ExpressionType.AndAlso, right);
+
+    /// <inheritdoc />
+    public PartiQLBinaryExpression Equal(PartiQLExpression left, PartiQLExpression right) =>
+        new(left, ExpressionType.Equal, right);
+
+    /// <inheritdoc />
+    public PartiQLBinaryExpression GreaterThan(PartiQLExpression left, PartiQLExpression right) =>
+        new(left, ExpressionType.GreaterThan, right);
+
+    /// <inheritdoc />
+    public PartiQLBinaryExpression GreaterThanOrEqual(PartiQLExpression left, PartiQLExpression right) =>
+        new(left, ExpressionType.GreaterThanOrEqual, right);
+
+    /// <inheritdoc />
+    public PartiQLBinaryExpression LessThan(PartiQLExpression left, PartiQLExpression right) =>
+        new(left, ExpressionType.LessThan, right);
+
+    /// <inheritdoc />
+    public PartiQLBinaryExpression LessThanOrEqual(PartiQLExpression left, PartiQLExpression right) =>
+        new(left, ExpressionType.LessThanOrEqual, right);
+    
+    /// <inheritdoc />
+    public PartiQLConstantExpression MakeConstant(object value, CoreTypeMapping? coreTypeMapping) =>
         new(Expression.Constant(value), coreTypeMapping);
 
-    public static PartiQLBinaryExpression NotEqual(PartiQLExpression left, PartiQLExpression right) =>
+    /// <inheritdoc />
+    public PartiQLBinaryExpression NotEqual(PartiQLExpression left, PartiQLExpression right) =>
         new(left, ExpressionType.NotEqual, right);
 
-    public static PartiQLBinaryExpression OrElse(PartiQLExpression left, PartiQLExpression right) =>
+    /// <inheritdoc />
+    public PartiQLBinaryExpression OrElse(PartiQLExpression left, PartiQLExpression right) =>
         new(left, ExpressionType.OrElse, right);
 }
