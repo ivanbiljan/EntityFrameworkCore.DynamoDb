3a852cdbd03123dee40d1feb4c97d2add688f682
ivanbiljan <ssbiljan@gmail.com>
2022-09-16 22:02:47 +0200

    Add DynamoDbQueryCompilationContext

--- src/DynamoDb.Linq/Internal/DynamoDbQueryCompilationContextFactory.cs
+++ src/DynamoDb.Linq/Internal/DynamoDbQueryCompilationContextFactory.cs
@@ -3,11 +3,21 @@ using Microsoft.EntityFrameworkCore.Query.Internal;
 
 namespace DynamoDb.Linq;
 
-internal class DynamoDbQueryCompilationContextFactory : QueryCompilationContextFactory
+internal class DynamoDbQueryCompilationContextFactory : IQueryCompilationContextFactory
 {
     private QueryCompilationContextDependencies _dependencies;
 
-    public DynamoDbQueryCompilationContextFactory(QueryCompilationContextDependencies dependencies) : base(dependencies)
+    public DynamoDbQueryCompilationContextFactory(QueryCompilationContextDependencies dependencies)
+    {
+        _dependencies = dependencies;
+    }
+
+    public QueryCompilationContext Create(bool async) => new DynamoDbQueryCompilationContext(_dependencies, async);
+}
+
+internal sealed class DynamoDbQueryCompilationContext : QueryCompilationContext
+{
+    public DynamoDbQueryCompilationContext(QueryCompilationContextDependencies dependencies, bool async) : base(dependencies, async)
     {
     }
 }
