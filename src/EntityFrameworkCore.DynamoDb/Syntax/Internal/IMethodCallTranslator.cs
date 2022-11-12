6d400d6af2fe285b38c4b543d756e5b0ef31fa79
ivanbiljan <ssbiljan@gmail.com>
2022-11-12 18:47:21 +0100

    Expose IMethodCallTranslator, add IMethodCallTranslatorProvider

--- src/EntityFrameworkCore.DynamoDb/Syntax/Internal/IMethodCallTranslator.cs
+++ src/EntityFrameworkCore.DynamoDb/Syntax/Internal/IMethodCallTranslator.cs
@@ -1,13 +1,15 @@
-﻿using System.Reflection;
+﻿using System.Linq.Expressions;
+using System.Reflection;
 using EntityFrameworkCore.DynamoDb.Syntax.Expressions;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace EntityFrameworkCore.DynamoDb.Syntax.Internal;
 
 /// <summary>
-///     Defines a contract that describes a type that translates .NET method calls into the DynamoDb equivalent. E.g.,
+///     Defines a contract that describes types that translate .NET method calls (<see cref="MethodCallExpression"/>) into their DynamoDb equivalents. E.g.,
 ///     <c>string.Contains(string)</c> into <c>CONTAINS</c>.
 /// </summary>
-internal interface IMethodCallTranslator
+public interface IMethodCallTranslator
 {
     /// <summary>
     ///     Takes an expression and produces the DynamoDb equivalent.
@@ -16,10 +18,69 @@ internal interface IMethodCallTranslator
     /// <param name="methodToTranslate">The method to translate.</param>
     /// <param name="arguments">The argument.</param>
     /// <returns>The resulting <see cref="PartiQLExpression" />.</returns>
-    PartiQLExpression Translate(
+    PartiQLExpression? Translate(
         PartiQLExpression source,
         MethodInfo methodToTranslate,
         IReadOnlyList<PartiQLExpression> arguments);
+}
+
+/// <summary>
+/// Defines a contract that describes modules consumers can register with Entity Framework Core to provide custom <see cref="IMethodCallTranslator"/>s.
+/// </summary>
+public interface IMethodCallTranslatorPlugin
+{
+    /// <summary>
+    /// Gets an enumerable collection of translators defined by this plugin.
+    /// </summary>
+    IEnumerable<IMethodCallTranslator> Translators { get; }
+}
+
+/// <summary>
+/// Defines a contract for types that evaluate <see cref="MethodCallExpression"/>s against a collection of <see cref="IMethodCallTranslator"/>s.
+/// </summary>
+internal interface IMethodCallTranslatorProvider
+{
+    
+    PartiQLExpression? Translate(
+        IModel model,
+        PartiQLExpression instance,
+        MethodInfo methodToTranslate,
+        IReadOnlyList<PartiQLExpression> arguments);
+}
+
+internal sealed class PartiQLMethodCallTranslatorProvider : IMethodCallTranslatorProvider
+{
+    private readonly List<IMethodCallTranslator> _defaultTranslators = new();
+    private readonly List<IMethodCallTranslator> _translatorsProvidedByPlugins = new();
+
+    public PartiQLMethodCallTranslatorProvider(IPartiQLExpressionFactory partiQLExpressionFactory, IEnumerable<IMethodCallTranslatorPlugin> translatorPlugins)
+    {
+        _defaultTranslators.AddRange(
+            new[]
+            {
+                new PartiQLStringMethodsTranslator(partiQLExpressionFactory)
+            });
+
+        _translatorsProvidedByPlugins.AddRange(translatorPlugins.SelectMany(p => p.Translators));
+    }
+
+    public PartiQLExpression? Translate(
+        IModel model,
+        PartiQLExpression instance,
+        MethodInfo methodToTranslate,
+        IReadOnlyList<PartiQLExpression> arguments)
+    {
+        foreach (var translator in _translatorsProvidedByPlugins.Concat(_defaultTranslators))
+        {
+            var translationResult = translator.Translate(instance, methodToTranslate, arguments);
+            if (translationResult is not null)
+            {
+                return translationResult;
+            }
+        }
+
+        return null;
+    }
 }
 
 /// <summary>
@@ -37,16 +98,11 @@ internal sealed class PartiQLStringMethodsTranslator : IMethodCallTranslator
         _partiQLExpressionFactory = partiQLExpressionFactory;
     }
 
-    public PartiQLExpression Translate(
+    public PartiQLExpression? Translate(
         PartiQLExpression source,
         MethodInfo methodToTranslate,
         IReadOnlyList<PartiQLExpression> arguments)
     {
-        if (source is null)
-        {
-            throw new ArgumentNullException(nameof(source));
-        }
-
         if (StartsWithMethodInfo.Equals(methodToTranslate))
         {
             return _partiQLExpressionFactory.BeginsWith(source, arguments[0]);
@@ -57,8 +113,6 @@ internal sealed class PartiQLStringMethodsTranslator : IMethodCallTranslator
             return _partiQLExpressionFactory.Contains(source, arguments[0]);
         }
 
-        throw new ArgumentException(
-            $"Cannot translate provided method: {methodToTranslate.Name}",
-            nameof(methodToTranslate));
+        return null;
     }
 }
